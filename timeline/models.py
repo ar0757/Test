ba6a86f5cfa1779e3b95f4f651d2de5d484c3bc8
Anand <ar0757@srmist.edu.in>
2023-09-08 18:31:10 +0400

    Slightly better Global Timeline

--- timeline/models.py
+++ timeline/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 from django.db import models
 
@@ -7,6 +7,6 @@ class TimelineEvent(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     def __str__(self):
         return self.title
