7c9ea16d13a5f131c0b2bd64205e381c1ea8a868
Anand <ar0757@srmist.edu.in>
2023-09-08 18:39:19 +0400

    Added Personal TImeline

--- timeline/urls.py
+++ timeline/urls.py
@@ -5,4 +5,5 @@ app_name = 'timeline'
 
 urlpatterns = [
     path('global/', views.global_timeline, name='global_timeline'),
+    path('personal/', views.personal_timeline, name='personal_timeline'),
 ]
